4f6be8481a8abe23c656a9a3dd2b2baa5114c7ac
stanislaumich <staslz42stas@gmail.com>
2021-03-19 12:18:59 +0300

    Start from work2

--- src/com/lz42/Main.java
+++ src/com/lz42/Main.java
@@ -1,8 +1,57 @@
 package com.Lz42;
+/*
+Patient: id, Фамилия, Имя, Отчество, Адрес, Телефон, Номер медицинской карты, Диагноз.
 
+Создать массив объектов. Вывести:
+
+a) список пациентов, имеющих данный диагноз;
+
+b) список пациентов, номер медицинской карты которых находится в заданном интервале.
+*/
+
+class Patient {
+    int id;
+    String lname;
+    String fname;
+    String oname;
+    String adress;
+    String phone;
+    int cnumber;
+    String diagnos;
+
+    void Patient(String plname,String pfname,String poname, String padress,
+                 String pphone, int pcnumber, String pdiagnos){
+        lname=plname;
+        fname=pfname;
+        oname=poname;
+        adress=padress;
+        phone=pphone;
+        cnumber=pcnumber;
+        diagnos=pdiagnos;
+    }
+    void Patient(String plname,String pfname,String poname, String padress,
+                 String pphone){
+        lname=plname;
+        fname=pfname;
+        oname=poname;
+        adress=padress;
+        phone=pphone;
+        cnumber=0;
+        diagnos="";
+    }
+
+    void displayInfo(){
+        System.out.printf("Name: %s \tPhone: %d\n", fname, phone);
+    }
+
+
+}
+
+//Patient[] PList;
+//
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello Java!");
     }
 }
